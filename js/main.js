efc473efbec6442ed7c89723d20949abe8254cdd
Andrea Prina <and.prina@gmail.com>
2022-06-16 15:21:01 +0200

    Completato Milestone 1

--- js/main.js
+++ js/main.js
@@ -1,9 +1,21 @@
-// Stampo in console i valori che vanno da 1 a 100 con un ciclo for
-// Se i multiplo di 3:
-    // Se i anche multiplo di 5 -> stampo FizzBuzz
-    // Altrimenti --> stampo Fizz
-// Se i multiplo di 5 -> stampo Buzz
-
 for (let i = 1; i <= 100; i++){
-    console.log(i);
+
+    // Check if the value is multiple of 3 and 5 - if yes, print "FizzBuzz"
+    if (i % 3 == 0 && i % 5 == 0){
+        console.log("FizzBuzz")
+    } else {
+
+        // Check if the value is multiple of 3 - if yes, print "Fizz"
+        if (i % 3 == 0){
+            console.log("Fizz")
+
+        // Check if the value is multiple of 5 - if yes, print "Buzz"
+        } else if (i % 5 == 0){
+            console.log("Buzz")
+
+        // Otherwhise, print the value
+        } else {
+            console.log(i)
+        }
+    }
 }
