749c1e920011da69d2e5ae4ee593fbffcc9bd0aa
Andrea Prina <and.prina@gmail.com>
2022-06-16 15:08:42 +0200

    Creato ciclo for per stampare i caratteri 1-100

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// Stampo in console i valori che vanno da 1 a 100 con un ciclo for
+// Se i multiplo di 3:
+    // Se i anche multiplo di 5 -> stampo FizzBuzz
+    // Altrimenti --> stampo Fizz
+// Se i multiplo di 5 -> stampo Buzz
+
+for (let i = 1; i <= 100; i++){
+    console.log(i);
+}
